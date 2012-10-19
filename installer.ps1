8bf03399d5737de47ee684d6963ae06dca0e35be
varadzhakov <varadjakov@gmail.com>
2012-10-19 11:09:19 +0300

    [#37836339] modify installer.ps1 to run the configuration injector

--- installer.ps1
+++ installer.ps1
@@ -1,3 +1,9 @@
 $pwd = [System.IO.Directory]::GetCurrentDirectory().Replace(" ","`` ")
+
+# import and executre configuration injector
+Import-Module ./ConfigurationInjector.dll 
+Set-Configuration -WorkingDirectory @pwd
+
+# install the services
 $command = "Start-Process '$psHome\powershell.exe' -Verb Runas -ArgumentList '" + $pwd + "\installerSub.ps1 -dir " + $pwd + "'"
 Invoke-Expression $command
